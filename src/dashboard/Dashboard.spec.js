975f35ac927d256dfe74e905208a2ff8c55112ad
LC Carrier <lc@LCs-MacBook-Pro.local>
2019-09-17 09:57:15 -0400

    updated files

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,29 @@
 // Test away
+
+import React from "react";
+import renderer from "react-test-renderer"; 
+import { render, fireEvent } from "@testing-library/react";
+import 'react-testing-library/cleanup-after-each';
+import Dashboard from './Dashboard'
+
+describe("<Dashboard />", () => {
+    it('renders at all', () => {
+        render(<Dashboard />)
+    });
+})
+
+describe("Dashboard display and controls tests", () => {
+    it('shows first render', () => {
+        const { getByText } = render(<Dashboard />);
+
+        expect(getByText('Lock Gate'));
+        expect(getByText('Close Gate'));
+    });
+
+    it('shows display on first render', () => {
+        const { getByText } = render(<Dashboard />);
+
+        expect(getByText('Unlocked'));
+        expect(getByText('Open'));
+    });
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,55 @@
 // Test away!
+import React from "react";
+import renderer from "react-test-renderer"; 
+import { render } from "@testing-library/react";
+import 'react-testing-library/cleanup-after-each';
+
+import Display from './Display'
+
+describe("<Display />", () => {
+    it('renders at all', () => {
+        render(<Display />)
+    });
+})
+
+describe("Display state tests based on props", () => {
+
+    it('shows unlocked/open', () => {
+        const display = render(<Display locked={false} closed={false}/>)
+        expect(display.getByText('Unlocked'))
+        expect(display.getByText('Open'))
+
+        const unlocked = display.getByText('Unlocked');
+        const open = display.getByText('Open');
+        expect(unlocked.className).toMatch('led green-led')
+        expect(open.className).toMatch('led green-led')
+    });
+    
+    it('shows unlocked/closed', () => {
+        const display = render(<Display locked={false} closed={true}/>)
+        expect(display.getByText('Unlocked'))
+        expect(display.getByText('Closed'))
+ 
+        const unlocked = display.getByText('Unlocked');
+        const closed = display.getByText('Closed');
+        expect(unlocked.className).toMatch('led green-led')
+        expect(closed.className).toMatch('led red-led')
+    });
+    
+    it('shows locked/closed', () => {
+        const display = render(<Display locked={true} closed={true}/>)
+        expect(display.getByText('Locked'))
+        expect(display.getByText('Closed'))
+
+        const locked = display.getByText('Locked');
+        const closed = display.getByText('Closed');
+        expect(locked.className).toMatch('led red-led')
+        expect(closed.className).toMatch('led red-led')
+    });
+
+    it('DOES NOT ever show locked/open', () => {
+        const display = render(<Display locked={true} closed={true}/>)
+        expect(display.queryByText('Locked')).toBeTruthy()
+        expect(display.queryByText('Open')).toBeFalsy()
+    });
+})
